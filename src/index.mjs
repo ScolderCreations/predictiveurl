b8f2f6dd362c5c93e88ec599a7b484feb7da94f7
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-12-09 00:23:31 +0000

    Fixed checker

--- src/index.mjs
+++ src/index.mjs
@@ -5,17 +5,24 @@ const predictiveurlconsts = {"prefixes": new Array("https://",
                            "ws://",
                            "wss://"
                            )};
+function containsPrefix() {
+  return new RegExp(
+    predictiveurlconsts.prefixes
+    .map(function (s) {return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');})
+    .join('{1,}|') + '{1,}'
+  );
+}
+
 
 function addPrefix(string, method, checkForPrefix) {
   try {var nstring = string} catch {throw new Error("addPrefix expected at least one value")}
   if (checkForPrefix) {
-    for (item in predictiveurlconsts.prefixes) {
-      if (string.includes(item)) {
-        return nstring
-      }
+    const contemp = containsPrefix()
+    if (contemp.test(nstring)) {
+      return nstring
     }
   }
-  if (method) {
+  if (method || (method = 0)) {
     try {nstring = predictiveurlconsts.prefixes[method] + nstring} catch(e) {throw new Error("request method cannot be added because of error:" + e)}
   } else {
     try {nstring = predictiveurlconsts.prefixes[1] + nstring} catch(e) {throw new Error("error occured while adding prefix:" + e)}
--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,7 @@
 import * as purl from "../src/index.mjs";
 
-console.log(purl.addPrefix("example.com"))
+console.log(purl.addPrefix("example.com", false, true))
 
+console.log(purl.addPrefix("blank", 2, true))
+
+console.log(purl.addPrefix("http://scratch.mit.edu/", false, true))
