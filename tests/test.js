d76e47676c105c9fa74df6afd6a677642f52da7a
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2021-12-14 08:56:12 -0800

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -8,3 +8,5 @@ console.log(purl.addPrefix("http://scratch.mit.edu/", false, true))
 
 console.log(purl.containsPrefix().test("wss://example.com"))
 console.log(purl.containsPrefix().test("I CAN DO ANYTHING"))
+
+console.log(purl.secure("http://scratch.mit.edu/"))
